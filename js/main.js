5e724d31017e32c784b0324c0ce227eb4727264b
ahmed14777 <ah9621777@gmail.com>
2023-04-07 15:18:22 +0200

    edit the code from mac
    git status
    clear

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //global declare
+console.log("hello");
 let body = document.body;
 
 //start username validation declare
@@ -21,8 +22,8 @@ const loginBtn = document.querySelector("#btn-login");
 let img = ["01.jpg", "02.jpg"];
 let randomNum = Math.floor(Math.random() * img.length);
 setInterval(function () {
-  let randomNum = Math.floor(Math.random() * img.length);
-  body.style.backgroundImage = `url(img/${img[randomNum]})`;
+    let randomNum = Math.floor(Math.random() * img.length);
+    body.style.backgroundImage = `url(./img/${img[randomNum]})`;
 }, 10000);
 // end random back ground
 //--------------------------------------------
@@ -30,51 +31,51 @@ setInterval(function () {
 // start check the email validation
 const userRegex = /^\S\w+\d*@\w+\.(com|net|org|it)/gi;
 addEventListener("focusout", () => {
-  // invalid email
-  if (userInput.value.match(userRegex) === null) {
-    userPassword.disabled = true; // disabled password
-    loginBtn.disabled = true; // disabled login
-    checker.innerHTML = "invalid";
-    userInput.style.color = "red";
-    userInput.style.boxShadow = "#ff0000a1 2px 2px 20px 0px";
-    checker.style.color = "red";
-  } else {
-    // valid email
-    checker.innerHTML = "valid";
-    userPassword.disabled = false; // able password
-    loginBtn.disabled = false; // able login
+    // invalid email
+    if (userInput.value.match(userRegex) === null) {
+        userPassword.disabled = true; // disabled password
+        loginBtn.disabled = true; // disabled login
+        checker.innerHTML = "invalid";
+        userInput.style.color = "red";
+        userInput.style.boxShadow = "#ff0000a1 2px 2px 20px 0px";
+        checker.style.color = "red";
+    } else {
+        // valid email
+        checker.innerHTML = "valid";
+        userPassword.disabled = false; // able password
+        loginBtn.disabled = false; // able login
 
-    userInput.style.color = "green";
-    userInput.style.boxShadow = "#00800096 0 0 20px 5px";
-    checker.style.color = "#00cd00";
-  }
-  // EMPTY INPUT
-  if (userInput.value === "") {
-    checker.innerHTML = "";
-    checker.style.color = "white";
-    userInput.style.boxShadow = "";
-    userInput.style.color = "#000";
-  }
-  // end check the email validation
+        userInput.style.color = "green";
+        userInput.style.boxShadow = "#00800096 0 0 20px 5px";
+        checker.style.color = "#00cd00";
+    }
+    // EMPTY INPUT
+    if (userInput.value === "") {
+        checker.innerHTML = "";
+        checker.style.color = "white";
+        userInput.style.boxShadow = "";
+        userInput.style.color = "#000";
+    }
+    // end check the email validation
 });
 //--------------------------------------------
 //--------------------------------------------
 addEventListener("focusout", () => {
-  if (userPassword.value.match(regexPass) === null) {
-    // invalid password
-    checkerPass.innerHTML = " minimum 8 charachter";
-    checkerPass.style.color = "red";
-    userPassword.style.boxShadow = "#ff0000a1 2px 2px 20px 0px";
-  } else {
-    // valid password
-    checkerPass.innerHTML = "valid password click login";
-    userPassword.style.boxShadow = "#00800096 0 0 20px 5px";
-    checkerPass.style.color = "green";
-  }
-  // EMPTY INPUT
-  if (userPassword.value === "") {
-    checkerPass.innerHTML = "";
-    checkerPass.style.color = "white";
-    userPassword.style.boxShadow = "";
-  }
+    if (userPassword.value.match(regexPass) === null) {
+        // invalid password
+        checkerPass.innerHTML = " minimum 8 characters";
+        checkerPass.style.color = "red";
+        userPassword.style.boxShadow = "#ff0000a1 2px 2px 20px 0px";
+    } else {
+        // valid password
+        checkerPass.innerHTML = "valid password click login";
+        userPassword.style.boxShadow = "#00800096 0 0 20px 5px";
+        checkerPass.style.color = "green";
+    }
+    // EMPTY INPUT
+    if (userPassword.value === "") {
+        checkerPass.innerHTML = "";
+        checkerPass.style.color = "white";
+        userPassword.style.boxShadow = "";
+    }
 });
